c4c7bdaae3a582c96d8e75f6970ab292df3f5cba
Kelli Libin <km.putnam2@gmail.com>
2023-05-17 16:02:13 -0600

    added sort, select, pagination

--- controllers/products.js
+++ controllers/products.js
@@ -3,13 +3,14 @@ const Product = require("../models/product");
 
 //controllers
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).sort('name');
+  const products = await Product.find({}).select("name price").limit(4);
   res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
   //mongoose 6+ automatically removes params that don't exist...can just pass in req.query to Product.find
-  const { featured, company, name, sort } = req.query;
+  //fields is made up - it's for the select method
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -23,16 +24,30 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
   let result = Product.find(queryObject);
-  if(sort) {
+  //check if sort param exists, then sort
+  if (sort) {
     //in postman it's set up where if you do multiple sorts, it's typed in as name,-price. we need name -price. ..but in postman i can write
     //it as the latter, so not entirely clear why this is necessary
-    const sortList = sort.split(',').join(' ')
-    result = result.sort(sortList)
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
   }
-  else {
-    result = result.sort('createdAt')
+  //check if fields param exists, then select method
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
   }
-  const products = await result
+  //set up pagination
+  //user passes in page, then multiply by the limit. so, for skip, user selects page 1, -1 = 0, ...essentially zero, means we skip zero items
+  //page 2 - 2-1 times limit of 10 = 10, means you skip 10.
+  //page / limit is what user passes in. access w req.query.page/limit
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1 ) * limit;
+  result = result.skip(skip).limit(limit)
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
