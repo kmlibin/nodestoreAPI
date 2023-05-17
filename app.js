2fa4313bc066568e41f25ef289b1e53d59a827f7
Kelli Libin <km.putnam2@gmail.com>
2023-05-17 11:32:36 -0600

    set up server, db, dotenv, middlewares, basic rout

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log('04 Store API')
+//dotenv
+require("dotenv").config();
+
+//async errors
+
+//server and database
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect"); //returns a promise, which is why the func it's consumed in is async
+
+//error middleware
+const notFoundMiddleWare = require("./middleware/not-found");
+const errorMiddleWare = require("./middleware/error-handler");
+
+//express middleware
+app.use(express.json());
+
+//routes
+
+app.get("/", (req, res) => {
+  res.send('<h1>StoreAPI</h1><a href="/api/v1/products">Product Page</a>');
+});
+
+//products route
+
+//errors
+app.use(notFoundMiddleWare);
+app.use(errorMiddleWare);
+
+//port variable
+const port = process.env.PORT || 3000;
+
+//connect to DB
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log("server is listening..."));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
+
+console.log("04 Store API");
